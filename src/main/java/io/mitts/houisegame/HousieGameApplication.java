0d1baaa82cf0c4260140ec386e49bdd8d0669478
msmittalas <msmittalas@gmail.com>
2020-05-03 01:32:10 +0530

    HousieGame inital commit
    added Notes which contains Backlogs to complete this project

---
+++ src/main/java/io/mitts/houisegame/HousieGameApplication.java
@@ -0,0 +1,13 @@
+package io.mitts.houisegame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HousieGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HousieGameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/mitts/houisegame/HousieGameApplicationTests.java
@@ -0,0 +1,13 @@
+package io.mitts.houisegame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HousieGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
