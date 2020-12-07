f1d1cc529f1f73ab9044bcfe6cfbbc70cac69f27
msmittalas <msmittalas@gmail.com>
2020-12-08 00:22:30 +0530

    home controller added

--- src/main/java/io/mitts/houisegame/mvc/controller/HomeMVCController.java
+++ src/main/java/io/mitts/houisegame/mvc/controller/HomeMVCController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
-@RestController("/")
+@RestController("/home")
 public class HomeMVCController {
 
 	
